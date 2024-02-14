c385e68a47f1d509f09fdef7020cb5871608e3ac
Andrew Forget <andrew.forget@andydragon.com>
2024-02-14 17:28:05 -0500

    Split the app into CloudSync vs not CloudSync

--- FeatureTracker/FeatureTrackerApp.swift
+++ FeatureTracker/FeatureTrackerApp.swift
@@ -8,6 +8,14 @@
 import SwiftData
 import SwiftUI
 
+struct CloudKitConfiguration {
+    #if CLOUDSYNC
+    static var Enabled = true
+    #else
+    static var Enabled = false
+    #endif
+}
+
 @main
 struct FeatureTrackerApp: App {
     private var modelContainer: ModelContainer = {
@@ -18,7 +26,7 @@ struct FeatureTrackerApp: App {
         let modelConfiguration = ModelConfiguration(
             schema: schema,
             isStoredInMemoryOnly: false,
-            cloudKitDatabase: .private("iCloud.com.andydragon.FeatureTracker"))
+            cloudKitDatabase: CloudKitConfiguration.Enabled ? .automatic : .none)
         
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
